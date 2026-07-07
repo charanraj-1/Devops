5647376083569c1f30c52b0ddb7f4a66c0979f95
charanraj_1 <kongaricharanraj@gmail.com>
2026-07-07 20:30:45 +0530

    updating iam_worker_node.tf

--- terraform/project2/iam_worker_node.tf
+++ terraform/project2/iam_worker_node.tf
@@ -26,3 +26,23 @@ resource "aws_iam_role" "node_role" {
 
   })
 }
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+
+  role = aws_iam_role.node_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+
+  role = aws_iam_role.node_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+
+  role = aws_iam_role.node_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
